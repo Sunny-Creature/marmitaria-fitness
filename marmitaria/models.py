87c503428091912e8d6aa8a976fffcdbbdb9c800
Sunny-Creature <soniaisissoninha119@gmail.com>
2026-05-20 11:03:07 -0300

    criando urls.py na pasta do app

--- marmitaria/models.py
+++ marmitaria/models.py
@@ -2,10 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Prato(models.Model):
+    MARMITA = "M"
+    LANCHE = "L"
+    ACOMPANHAMENTO = "A"
+    SALGADO = "S"
+    DOCE = "D"
+    CATEGORIAS_PRATOS = {
+        MARMITA: "Marmita",
+        LANCHE: "Lanche",
+        ACOMPANHAMENTO: "Acompanhamento",
+        SALGADO: "Salgado",
+        DOCE: "doce",
+    }
+
+
+    CATEGORIAS_MARMITA = {
+    }
+
     nome_prato = models.CharField(max_length=50)
     descricao = models.TextField()
     quant_calorias = models.IntegerField()
     img_prato = models.ImageField(upload_to="img_pratos/")
-
-class Categoria(models.Model):
-    nome_categoria = models.CharField(max_length=50)
